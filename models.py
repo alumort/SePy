895a47b047966a4a34e42f40d18fec5f85cdff22
Sabrina Mihocka Viglietti <alanissabrinamv@gmail.com>
2026-09-21 14:41:56 -0300

    [AS] starting cart and cart product classes

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Float, String, Integer
+from sqlalchemy import create_engine, Column, Float, String, Integer, Date
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
@@ -14,6 +14,22 @@ class Product(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String, nullable=False)
     price = Column(Float, nullable=False)
+
+class Cart(Base):
+    __tablename__ = "carritos"
+    __table_args__ = {"sqlite_autoincrement": True}
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    created_at = Column(Date, default=Date.now)
+    status = Column(String, nullable=False)
+
+class Cart_Product(Base):
+    __tablename__ = "carrito_producto"
+    __table_args__ = {"sqlite_autoincrement": True}
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id_cart = Column(Integer, foreign_key = Cart, nullable=False)
+    id_product = Column(Integer, foreign_key = Product, nullable=False)
+    quantity = Column(Integer, nullable=False)
+
 
 
 Base.metadata.create_all(engine)
