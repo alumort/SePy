547f8a2f07a7951757f0067a02e1a0d98918c6e1
Sabrina Mihocka Viglietti <alanissabrinamv@gmail.com>
2026-09-22 22:42:36 -0300

    [AS] fixed cart and cart product classes, added sale class

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Float, String, Integer, Date
+from sqlalchemy import create_engine, Column, Float, String, Integer, Date, ForeignKey, Time
 from sqlalchemy.orm import declarative_base, sessionmaker
-
+from datetime import datetime
 
 engine = create_engine("sqlite:///products.db", echo=True)
 
@@ -10,24 +10,31 @@ Base = declarative_base()
 class Product(Base):
     __tablename__ = "productos"
     __table_args__ = {"sqlite_autoincrement": True}
-
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String, nullable=False)
     price = Column(Float, nullable=False)
+
+class Sale(Base):
+    __tablename__ = "ventas"
+    __table_args__ = {"sqlite_autoincrement": True}
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    date = Column(Date, default=datetime.now, nullable=False)
+    time = Column(Time, default=datetime.now, nullable=False)
+    id_cart = Column(Integer, ForeignKey("carritos.id"), nullable=False, unique=True)
 
 class Cart(Base):
     __tablename__ = "carritos"
     __table_args__ = {"sqlite_autoincrement": True}
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    created_at = Column(Date, default=Date.now)
+    created_at = Column(Date, default=datetime.now, nullable=False)
     status = Column(String, nullable=False)
 
 class Cart_Product(Base):
     __tablename__ = "carrito_producto"
     __table_args__ = {"sqlite_autoincrement": True}
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    id_cart = Column(Integer, foreign_key = Cart, nullable=False)
-    id_product = Column(Integer, foreign_key = Product, nullable=False)
+    id_cart = Column(Integer, ForeignKey("carritos.id"), nullable=False)
+    id_product = Column(Integer, ForeignKey("productos.id"), nullable=False)
     quantity = Column(Integer, nullable=False)
 
 
